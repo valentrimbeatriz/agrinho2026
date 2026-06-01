170a07e5aa723db81205f552da83e9205c507a3e
valentrimbeatriz <valentim.beatriz@escola.pr.gov.br>
2026-06-01 08:04:26 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function abrirQuiz() {
+  alert("Quiz sobre sustentabilidade em breve");
+}
+function abrirCalculadora(){
+  let comsumoAgua = prompt("Digite o consumo de água (litros):");
+  let impacto = consumoAgua * 0.001;// cálculo simples
+alert("Impacto ambiental estimado: " + impacto + "toneladas de CO2 equivalente.");
+}
